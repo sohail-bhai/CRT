464d919d9fc98d75c81ac97bf79042b9e9e20735
sohail-bhai <sohailahmedwork87@gmail.com>
2026-01-07 09:17:27 +0530

    new file

---
+++ IV th sem/Python/PRACTICE/M01/S01/PS01.py
@@ -0,0 +1,31 @@
+"""
+x=10
+y=12.45
+z=5+6j
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+f1=12e2
+f2=5E3
+print(f1,type(f1))
+print(f2,type(f2))
+
+c1=5+ 4j
+c2=complex(2,-3)
+print(c1,c2)
+print(c1+c2)
+print(c1-c2)
+print(c1*c2)
+print(c1/c2)
+print(c1.real)
+print(c1.imag) 
+"""
+s1="python"
+s2='python 3.12.2'
+s3='''line-1
+line-2
+line-3
+'''
+print(s1[3])
+print(s1[::2])
+print(s1[::-1])
